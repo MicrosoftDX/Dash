d8097bb1f2cd7d3084a2a4705278f476e71105c8
jamesbak@microsoft.com <jamesbak@microsoft.com>
2014-10-27 14:37:50 -0700

    Added blob categorization with associated controller

---
+++ DashServer/Controllers/BlobController.cs
@@ -0,0 +1,150 @@
+﻿//     Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Microsoft.Dash.Server.Controllers
+{
+    [RoutePrefix("Blob")]
+    public class BlobController : ApiController
+    {
+        /// Get Blob - http://msdn.microsoft.com/en-us/library/azure/dd179440.aspx
+        [HttpGet]
+        public async Task<IHttpActionResult> GetBlob(string container, string blob, string snapshot = null)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Put Blob - http://msdn.microsoft.com/en-us/library/azure/dd179451.aspx
+        [HttpPut]
+        public async Task<IHttpActionResult> GetBlob(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Delete Blob - http://msdn.microsoft.com/en-us/library/azure/dd179413.aspx
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteBlob(string container, string blob, string snapshot = null)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Get Blob Properties - http://msdn.microsoft.com/en-us/library/azure/dd179394.aspx
+        [HttpHead]
+        public async Task<IHttpActionResult> GetBlobProperties(string container, string blob, string snapshot = null)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Get Blob operations with the 'comp' query parameter
+        [AcceptVerbs("GET", "HEAD")]
+        public async Task<IHttpActionResult> GetBlobComp(string container, string blob, string comp, string snapshot = null)
+        {
+            switch (comp.ToLower())
+            {
+                case "metadata":
+                    return await GetBlobMetadata(container, blob, snapshot);
+
+                case "blocklist":
+                    return await GetBlobBlockList(container, blob, snapshot);
+
+                default:
+                    return BadRequest();
+            }
+        }
+
+        /// PUT Blob operations with the 'comp' query parameter
+        [HttpPut]
+        public async Task<IHttpActionResult> PutBlobComp(string container, string blob, string comp)
+        {
+            switch (comp.ToLower())
+            {
+                case "properties":
+                    return await SetBlobProperties(container, blob);
+
+                case "metadata":
+                    return await SetBlobMetadata(container, blob);
+
+                case "lease":
+                    return await LeaseBlob(container, blob);
+
+                case "snapshot":
+                    return await SnapshotBlob(container, blob);
+
+                case "block":
+                    return await PutBlobBlock(container, blob);
+
+                case "blocklist":
+                    return await PutBlobBlockList(container, blob);
+
+                default:
+                    return BadRequest();
+            }
+        }
+
+        /// Set Blob Properties - http://msdn.microsoft.com/en-us/library/azure/ee691966.aspx
+        private async Task<IHttpActionResult> SetBlobProperties(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Get Blob Metadata - http://msdn.microsoft.com/en-us/library/azure/dd179350.aspx
+        private async Task<IHttpActionResult> GetBlobMetadata(string container, string blob, string snapshot)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Set Blob Metadata - http://msdn.microsoft.com/en-us/library/azure/dd179414.aspx
+        private async Task<IHttpActionResult> SetBlobMetadata(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Lease Blob - http://msdn.microsoft.com/en-us/library/azure/ee691972.aspx
+        private async Task<IHttpActionResult> LeaseBlob(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Snapshot Blob - http://msdn.microsoft.com/en-us/library/azure/ee691971.aspx
+        private async Task<IHttpActionResult> SnapshotBlob(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Get Block List - http://msdn.microsoft.com/en-us/library/azure/dd179400.aspx
+        private async Task<IHttpActionResult> GetBlobBlockList(string container, string blob, string snapshot)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Put Block - http://msdn.microsoft.com/en-us/library/azure/dd135726.aspx
+        private async Task<IHttpActionResult> PutBlobBlock(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+
+        /// Put Block List - http://msdn.microsoft.com/en-us/library/azure/dd179467.aspx
+        private async Task<IHttpActionResult> PutBlobBlockList(string container, string blob)
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+    }
+}
