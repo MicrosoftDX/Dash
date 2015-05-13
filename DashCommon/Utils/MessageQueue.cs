02018f97e762a18fcb0e293cf1512dbd21adc889
Elliott Miller <emil@microsoft.com>
2015-05-13 11:28:57 -0700

    Adding DashAsync project, adding MessageQueue interface, Fixing SDK reference

---
+++ DashCommon/Utils/MessageQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Dash.Common.Utils
+{
+    public interface IMessage
+    {
+        T GetPayload<T>();
+    }
+    public interface IMessageQueue
+    {
+        public void Enqueue(IMessage message);
+        public IMessage Dequeue();
+        public IMessage Peek();
+    }
+    class MessageQueue
+    {
+    }
+}
