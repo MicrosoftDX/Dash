5e8b76ca9b4882553d27ed5722f32c2b6ff38cea
Jason <github@jasonpoon.ca>
2015-05-18 11:56:43 -0700

    Add logging of web api messages

--- DashServer.Tests/WebApiTestRunner.cs
+++ DashServer.Tests/WebApiTestRunner.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
@@ -17,7 +18,7 @@ namespace Microsoft.Tests
 {
     class WebApiTestRunner 
     {
-        HttpClient _requestClient;
+        readonly HttpClient _requestClient;
 
         public WebApiTestRunner(IDictionary<string, string> config = null)
         {
@@ -57,6 +58,8 @@ namespace Microsoft.Tests
 
         public HttpResponseMessage ExecuteRequestWithHeaders(string uri, string method, HttpContent content, IEnumerable<Tuple<string, string>> headers, HttpStatusCode expectedStatusCode = HttpStatusCode.Unused)
         {
+            Trace.WriteLine(String.Format("Method={0}. Uri={1}.", method, uri));
+
             HttpResponseMessage retval = null;
             SetupRequest(uri, method);
             HttpRequestMessage request;
@@ -99,7 +102,7 @@ namespace Microsoft.Tests
 
                 default:
                     // Unsupported method
-                    System.Diagnostics.Debug.Assert(false);
+                    Debug.Assert(false);
                     break;
             }
             if (expectedStatusCode != HttpStatusCode.Unused && retval != null)
