1b0964f6fdc386cf8a618324ab25d6ff8fbaf4a7
Elliott Miller <emil@microsoft.com>
2015-12-15 13:50:35 -0800

    Adding in INavRoute

---
+++ DashServer.ManagementAPI/Scripts/typings/adal/INavRoute.d.ts
@@ -0,0 +1,15 @@
+﻿declare module adal.shared {
+
+    /**
+     * Interface for navigation routes.
+     */
+    export interface INavRoute {
+        title?: string;
+        controller?: string|Function;
+        templateUrl?: string;
+        controllerAs?: string;
+        requireADLogin?: boolean;
+        showInNav?: boolean;
+    }
+
+} 
